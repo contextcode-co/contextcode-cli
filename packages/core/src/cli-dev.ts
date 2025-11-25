dfc0577dbaf26969b714c76b4b589e8fc81483b8
Juliano Firme <julianofirme23@gmail.com>
2025-11-25 10:56:34 -0300

    feat: remove deprecated core components and refactor indexing and rendering logic

--- packages/core/src/cli-dev.ts
+++
@@ -1,6 +0,0 @@
-import { indexRepo } from "./indexer.js";
-const cwd = process.cwd();
-(async () => {
-  const res = await indexRepo(cwd);
-  console.log("Index result:", JSON.stringify(res, null, 2));
-})();
